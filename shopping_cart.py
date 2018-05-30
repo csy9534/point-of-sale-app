b4dfa6a5390cca9519c8a1920d6c012a9ca5e1aa
csy9534 <39348132+csy9534@users.noreply.github.com>
2018-05-30 14:23:35 -0400

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,7 +1,6 @@
 # shopping_cart.py
 
 import datetime
-import os
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
